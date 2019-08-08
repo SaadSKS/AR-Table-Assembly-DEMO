7ad70eedcf229481f9a8dea75e182a8cf35817b7
SaadSKS <saad.sks@gmail.com>
2019-08-08 12:38:44 +0530

    Model Ready

---
+++ AR Assembly Demo/Assets/Scripts/MenuInterface.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuInterface : MonoBehaviour
+{
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
